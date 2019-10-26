8f133b9d8e7ab583d6f81f15bb94f1f158f4af5a
saiesaire <rozenkreuz1126@gmail.com>
2019-10-26 13:30:03 +0900

    非同期通信

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,17 @@
 $(function(){
   $('#user-search-field').on(`keyup`, function(){
     var input = $("#user-search-field").val();;    //フォームの値を取得して変数に代入する
-    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      dataType: 'json',
+      data: { keyword: input},
+    })
+      .done (function(users){
+        console.log("成功です");
+      })
+      .fail (function(users){
+        console.log("失敗です");
+      })
   })
 });
