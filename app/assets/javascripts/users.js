195225c3dea68790e6ff2a196334430efa6447fc
saiesaire <rozenkreuz1126@gmail.com>
2019-10-26 14:24:38 +0900

    HTML表示・エラー表示

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,17 +1,54 @@
 $(function(){
+  function addUser(user) {
+    var html = `
+      <div class = "chat-group-user cleafix>
+        <p class = "chat-group-user__name">${user.name}</p>
+        <div class = "user-serch-add chat-group-user__btn chat-group-user__btn--add" data-user-if = "${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-serch-result").append(html);
+  }
+
+  function addNoUser() {
+    var html = `
+      <div class = "chat-group-user cleafix">
+        <p class = "chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-serch-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    var html = `
+      <div class = "ChatMember cleafix" if = "${id}>
+        <p class = "ChatMember__name>${name}</p>
+        <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+      </div>
+    `;
+    $("#user-serch-result").append(html);
+  }
   $('#user-search-field').on(`keyup`, function(){
-    var input = $("#user-search-field").val();;    //フォームの値を取得して変数に代入する
+    var input = $('#user-search-field').val(); 
     $.ajax({
       type: "GET",
       url: "/users",
-      dataType: 'json',
       data: { keyword: input},
+      dataType: "json"
     })
-      .done (function(users){
-        console.log("成功です");
+      .done(function(users) {
+        $('#user-serch-result').empty();
+        
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            addfUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
       })
-      .fail (function(users){
-        console.log("失敗です");
-      })
-  })
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示できません。");
+      });
+  });
 });
