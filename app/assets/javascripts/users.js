7353b0e07c21d521eff9e629e3248bee074be72a
saiesaire <rozenkreuz1126@gmail.com>
2019-10-27 12:23:42 +0900

    error fix
    HTML描画

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,45 +1,44 @@
-$(function(){
+$(function() {
   function addUser(user) {
-    var html = `
-      <div class = "chat-group-user cleafix>
-        <p class = "chat-group-user__name">${user.name}</p>
-        <div class = "user-serch-add chat-group-user__btn chat-group-user__btn--add" data-user-if = "${user.name}">追加</div>
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>
     `;
-    $("#user-serch-result").append(html);
+    $("#user-search-result").append(html);
   }
 
   function addNoUser() {
-    var html = `
-      <div class = "chat-group-user cleafix">
-        <p class = "chat-group-user__name">ユーザーが見つかりません</p>
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
       </div>
     `;
-    $("#user-serch-result").append(html);
+    $("#user-search-result").append(html);
   }
   function addDeleteUser(name, id) {
-    var html = `
-      <div class = "ChatMember cleafix" if = "${id}>
-        <p class = "ChatMember__name>${name}</p>
-        <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
-      </div>
-    `;
-    $("#user-serch-result").append(html);
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class="ChatMember__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".ChatMembers").append(html);
   }
-  $('#user-search-field').on(`keyup`, function(){
-    var input = $('#user-search-field').val(); 
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
     $.ajax({
       type: "GET",
       url: "/users",
-      data: { keyword: input},
+      data: { keyword: input },
       dataType: "json"
     })
       .done(function(users) {
-        $('#user-serch-result').empty();
-        
+        $("#user-search-result").empty();
+
         if (users.length !== 0) {
-          users.forEach(function(user){
-            addfUser(user);
+          users.forEach(function(user) {
+            addUser(user);
           });
         } else if (input.length == 0) {
           return false;
