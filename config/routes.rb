bbda0beab7cf1c1921e9e9f6cf99386d1f046087
saiesaire <rozenkreuz1126@gmail.com>
2019-11-02 17:50:24 +0900

    pre-comp

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update,:index]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:new, :create, :index, :edit, :update] do
     resources :messages, only: [:index, :create]
+
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
