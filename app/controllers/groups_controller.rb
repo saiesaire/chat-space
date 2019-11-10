f4f4c622b6e5745fe0b9b26f6e30f96712c3b27e
saiesaire <rozenkreuz1126@gmail.com>
2019-11-10 17:53:03 +0900

    group update fix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,24 +1,25 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
   
   def new
     @group = Group.new
-    @group.users << current_user
+  end
+
+  def edit
+    @group = Group.find(params[:id])
   end
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
-  end
-
-  def edit
-    @group = Group.find(params[:id])
   end
 
   def update
@@ -31,7 +32,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name,{ :user_ids => [] })
   end
 
   def set_group
